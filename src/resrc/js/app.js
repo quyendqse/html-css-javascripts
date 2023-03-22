67ef788b25d41d9d56c3e22324bd0282f0231e54
Quyen Dao <quyendqse150975@fpt.edu.vn>
2023-03-22 19:59:03 +0700

    update/code

--- src/resrc/js/app.js
+++ src/resrc/js/app.js
@@ -16,6 +16,6 @@ $(document).ready(function(){
     slidesToShow: 3,
     slidesToScroll: 1,
     prevArrow:"<button type='button' class='slick-prev slick-arrow'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-  nextArrow:"<button type='button' class='slick-next slick-arrow'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+   nextArrow:"<button type='button' class='slick-next slick-arrow'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
 
   });
