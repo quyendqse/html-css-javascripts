8f4d0e5217d91fbdfbf81037bcf781d7807b3667
user.name like Nekiii12 <user.email zinsike444@gmail.com>
2023-03-24 19:12:25 +0700

    Create about.js

---
+++ src/resrc/js/about.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.all-service-items').slick({
+        slidesToShow: 4 ,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows : false,
+       
+    });
+  });
+ 
+  $('.background7').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow:"<button type='button' class='slick-prev slick-arrow'><i class='icofont-rounded-left' aria-hidden='true'></i></button>",
+   nextArrow:"<button type='button' class='slick-next slick-arrow'><i class='icofont-rounded-right' aria-hidden='true'></i></button>",
+
+  });
