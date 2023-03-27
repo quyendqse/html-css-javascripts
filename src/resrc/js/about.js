9d3d9eaad9d7d4c7fb7c81a9c967aaf8cdaf3348
Quyen Dao <quyendqse150975@fpt.edu.vn>
2023-03-27 17:43:17 +0700

    update/code

--- src/resrc/js/about.js
+++ src/resrc/js/about.js
@@ -1,13 +1,4 @@
-$(document).ready(function(){
-    $('.all-service-items').slick({
-        slidesToShow: 4 ,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        arrows : false,
-       
-    });
-  });
+
  
   $('.background7').slick({
     autoplay: true,
